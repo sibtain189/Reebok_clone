510ebbabfd2ba10fe91d08324baea01b2be21490
Primahale <mahalepriyanka82@gmail.com>
2022-01-23 12:14:04 +0530

    final changes

--- CartPage/cart.js
+++ CartPage/cart.js
@@ -152,3 +152,16 @@ function checkLogin(){
         window.location.href="http://127.0.0.1:5501/LoginPage/login.html"
     }
 }
+
+
+function Login(){
+    window.location.href="http://127.0.0.1:5501/LoginPage/login.html"
+  }
+  
+  function goToCart(){
+    window.location.href="http://127.0.0.1:5501/CartPage/cart.html"
+  }
+  
+  function goToHome(){
+    window.location.href="http://127.0.0.1:5501/HomePage/home.html"
+  }
