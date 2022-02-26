8f27896973d988061e06612f28d00ccab0e44425
Primahale <mahalepriyanka82@gmail.com>
2022-02-26 13:42:46 +0530

    Update cart.js

--- CartPage/cart.js
+++ CartPage/cart.js
@@ -164,6 +164,7 @@ function Login(){
     window.location.href="http://127.0.0.1:5501/CartPage/cart.html"
   }
   
+  
   function goToHome(){
     window.location.href="http://127.0.0.1:5501/HomePage/home.html"
   }
